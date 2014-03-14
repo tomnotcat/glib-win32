a4349e3ae43cd774ab92c0cddc044d8029e79ef4
tomnotcat <tomnotcat@163.com>
2014-03-14 14:34:42 +0800

    Add some static libs

--- vs10/gettext/libgnuintl.h
+++ vs10/gettext/libgnuintl.h
@@ -19,10 +19,14 @@
 #ifndef _LIBINTL_H
 #define _LIBINTL_H 1
 
+#if 1 && LIBINTL_STATIC
+#define LIBINTL_DLL_EXPORTED
+#else
 #if 1 && BUILDING_LIBINTL
 #define LIBINTL_DLL_EXPORTED __declspec(dllexport)
 #else
 #define LIBINTL_DLL_EXPORTED __declspec(dllimport)
+#endif
 #endif
 
 #include <locale.h>
